8d23e53d0859aa52e52c187bc8fb1ba415ded0a3
Angel Rodriguez <92960380+BacalaoYT@users.noreply.github.com>
2025-05-03 19:04:03 -0600

    Add files via upload

--- Placas.cpp
+++ Placas.cpp
@@ -3,26 +3,11 @@ using namespace std;
 
 #define MAXLETRA 21
 
-const char letrasPermitidas[MAXLETRA] = { // Definimos el abecedario solo con CONSONANTES
-    'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M',
-    'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'
-};
-
 struct Matriculas{ // Hacemos un incremento para el caracter nulo que se agrega
     char numeros[5];
     char letras[4];
 
 } matricula;
-
-//Esta funcion analiza la posicion de la letra dentro de  "Letras Permitidas"
-int posicionLetra(char letra){
-    for (int i = 0; i < MAXLETRA; i++){
-        if (letrasPermitidas[i] == letra)
-            return i;
-    }
-    return -1; /*Al retronar -1 damos a entender que no se encontro un valor,
-                si colocamos el 1 puede interpretarse que el valor esta en esa posicion*/
-}
 
 int main(){
 
